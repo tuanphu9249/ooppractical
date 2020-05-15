28d7fc056df8d4dcb342d0f1398c6be37e01eafe
antoine.phu <tuanphu9249@gmail.com>
2020-05-15 23:34:24 +0930

    AirportTester.cpp added

---
+++ AirportTester.cpp
@@ -0,0 +1,14 @@
+
+
+
+int main()
+{
+    Airport* melbourne = new Airport("Melbourne");
+    Airport* adelaidu = new Airport("Adelaidu");
+
+    Flight* flight1 = new Flight(melbourne, adelaidu, ....);
+
+    Ticket* ticket1 = new Ticket(flight1, "bo may")
+
+    
+}
