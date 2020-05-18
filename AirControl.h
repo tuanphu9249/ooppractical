47a1b689d063e6e3d60dac4eee9e18206362363e
antoine.phu <tuanphu9249@gmail.com>
2020-05-19 00:44:05 +0930

    update AirControl.h API

--- AirControl.h
+++ AirControl.h
@@ -12,14 +12,23 @@ class AirControl
     private:
         Airport* airports;
         static int numAirports;
-
+        int[50][50] costs;
     public:
         AirControl();
 
 
         FlightType* flightCheck(string arrival, string destination); // show flight ID, name, # of passengers,
                                                                     // price, speed, time of all flights available
-        bool buyTicket(string arrival, string destination);         
+        bool buyTicket(string arrival, string destination, Ticket* ticket);    
+
+        Airport* cheapeastRoute(string arrival, string destination);    // return the cheapeast route
+                                                                        // between arrival and destination as an array of airports
+
+        int bestRoute(string arrival, string destination);              // return the cheapeast cost between arrival and destination
+
+    private:
+
+
 
 
 
