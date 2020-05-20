087283efa7d3925ee5b70542d8030a497c6f6306
quanghuy17111999@gmail.com <quanghuy17111999@gmail.com>
2020-05-20 12:43:09 +0930

    add FLightTester.cpp and ExpectedFlightTestOutput01.txt

--- FlightTester.cpp
+++ FlightTester.cpp
@@ -2,11 +2,38 @@
 using namespace std;
 #include "Flight.h"
 #include "Airport.h"
-
+#include <vector>
 
 int main()
 {
     // unit testing
+    {
+        vector<Flight*> gate1; 
+        vector<Flight*>::iterator it;
+        Flight* flight1 = new Flight("JS-110","Adelaide","Sydney",10,8);
+        Flight* flight2 = new Flight("DS-112", "Darwin","Kuala Lumpur",9,10);
+        Flight* flight3 = new Flight("VJ-303", "Melbourne", "Budapest",20,11);
+        Flight* flight4 = new Flight("MH-370","Kuala Lampur","Beijing",23,15);
+        gate1.push_back(flight1);
+        gate1.push_back(flight2);
+        gate1.push_back(flight3);
+        gate1.push_back(flight4);
+        for(it = gate1.begin(); it < gate1.end(); it++)
+        {
+            cout << (*it)->getName() << " " << (*it)->getDeparture() << " " 
+            << (*it)->getArrival() << " " << (*it)->getBasePrice() << " " << (*it)->getDuration() << endl;
+        }
+        flight1->setArrival("Melbourne");
+        flight2->setName("VL-091");
+        for(it = gate1.begin(); it < gate1.end(); it++)
+        {
+            cout << (*it)->getName() << " " << (*it)->getDeparture() << " " 
+            << (*it)->getArrival() << " " << (*it)->getBasePrice() << " " << (*it)->getDuration() << endl;
+        }
+        flight3->setCurBusinessSeat(10);
+        flight3->setCurEcoSeat(5);
+        cout << flight3->getCurBusinessSeat() << " " << flight3->getCurEcoSeat() <<endl;
 
+    }   
     
 }
