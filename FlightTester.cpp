083aebcc10f00809658fab019529b2fef434800c
antoine.phu <tuanphu9249@gmail.com>
2020-05-27 00:23:34 +0930

    final-final

--- FlightTester.cpp
+++ FlightTester.cpp
@@ -11,10 +11,10 @@ int main()
     {
         vector<Flight*> gate1; 
         vector<Flight*>::iterator it;
-        Flight* flight1 = new Flight("JS-110","Adelaide","Sydney",10,8);
-        Flight* flight2 = new Flight("DS-112", "Darwin","Kuala Lumpur",9,10);
-        Flight* flight3 = new Flight("VJ-303", "Melbourne", "Budapest",20,11);
-        Flight* flight4 = new Flight("MH-370","Kuala Lampur","Beijing",23,15);
+        Flight* flight1 = new Flight("JS-110","ADELAIDE","SYDNEY",10,8);
+        Flight* flight2 = new Flight("DS-112", "DARWIN","KUALA LUMPUR",9,10);
+        Flight* flight3 = new Flight("VJ-303", "MELBOURNE", "BUDAPEST",20,11);
+        Flight* flight4 = new Flight("MH-370","KUALA LAMPUR","BEIJING",23,15);
         gate1.push_back(flight1);
         gate1.push_back(flight2);
         gate1.push_back(flight3);
@@ -24,7 +24,7 @@ int main()
             cout << (*it)->getName() << " " << (*it)->getDeparture() << " " 
             << (*it)->getArrival() << " " << (*it)->getBasePrice() << " " << (*it)->getDuration() << endl;
         }
-        flight1->setArrival("Melbourne");
+        flight1->setArrival("MELBOURNE");
         flight2->setName("VL-091");
         for(it = gate1.begin(); it < gate1.end(); it++)
         {
@@ -42,11 +42,6 @@ int main()
         for (int i = 0; i < 115; i++)
             flight->addTicket(new Ticket(flight->getName(), "Anh Tuan Phu", "14/08/99", i, 30));
 
-        Ticket* tickets = flight->getTickets();
-        for (int i = 0; i < 115; i++)
-        {
-            cout << tickets[i].getName() << " " << tickets[i].getFlightName() << endl;
-        }
 
         if (flight->getNumTicket() != 115)
             cout << "Error with Flight::addTicket()" << endl;
